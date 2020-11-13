4d3c99d8feb011489c2f26288fbbd5e21d166e37
Ryanlawton001 <56266188+Ryanlawton001@users.noreply.github.com>
2020-11-13 16:03:29 +0000

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Auth::routes();
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Route::post('send', [\App\Http\Controllers\ChatController::class, 'send']);
 
-Route::get('chat', [\App\Http\Controllers\ChatController::class, 'chat']);
+Route::get('/', [\App\Http\Controllers\ChatController::class, 'chat']);
 
 Route::post('saveToSession', [\App\Http\Controllers\ChatController::class, 'saveToSession']);
 
